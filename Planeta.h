c36aebebbf22324cfbe390f3926f5faa128daa6c
Luis Gaytan <a01771191@tec.mx>
2025-10-24 12:58:35 -0600

    Refactor Planeta.h to improve readability

--- Planeta.h
+++ Planeta.h
@@ -1,21 +1,29 @@
+// definir proteccion de inclusion
 #ifndef PLANETA_H
 #define PLANETA_H
 
+// incluir string
 #include <string>
 
 // declarar la clase planeta
 class Planeta {
     // declarar atributos
     std::string nombre;
+    // declarar atributos
     int anio;
+    // declarar atributos
     double distancia;
+    // declarar atributos
     double masa;
+    // declarar atributos
     double radio;
+    // declarar atributos
     std::string metodo;
 
 public:
-    // declarar constructores
+    // declarar constructor por defecto
     Planeta();
+    // declarar constructor con parametros
     Planeta(const std::string& nombre,
             int anio,
             double distancia,
@@ -25,10 +33,15 @@ public:
 
     // declarar metodos getters
     const std::string& getNombre() const;
+    // declarar metodos getters
     int getAnio() const;
+    // declarar metodos getters
     double getDistancia() const;
+    // declarar metodos getters
     double getMasa() const;
+    // declarar metodos getters
     double getRadio() const;
+    // declarar metodos getters
     const std::string& getMetodo() const;
 
     // declarar metodo para texto legible
@@ -38,4 +51,5 @@ public:
     static Planeta crearDesdeCSV(const std::string& linea);
 };
 
-#endif // PLANETA_H
+// terminar proteccion de inclusion
+#endif
